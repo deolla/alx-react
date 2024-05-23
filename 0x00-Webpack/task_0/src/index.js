bce498d77380436ed4dd8f82bf8ad270aea9ca9e
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-05-23 21:56:59 +0100

    web_pack

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(document).ready(function () {
+    $('body').append('<p>Holberton Dasboard</p>');
+    $('body').append('<p>Dashboard data for the students</P>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+});
