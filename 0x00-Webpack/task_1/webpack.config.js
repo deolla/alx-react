c7eecded5504bf3dc05d47c73f2aa27a34c7cc41
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-05-24 13:43:48 +0100

    WebPack

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/dashboard_main.js',
+    entry: '../js/dashboard_main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
