cc573ddf0d03f19250fa9cde78ec0ec4d2c0d5e3
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-05-24 13:27:16 +0100

    WebPack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,21 +1,19 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-// Append elements to the body
-$('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the students</p>");
-$('body').append("<button id='start-button'>Click here to get started</button>");
-$('body').append("<p id='count'></p>");
-$('body').append("<p>Copyright - Holberton School</p>");
+$(document).ready(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="start-btn">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
 
-// Initialize click counter
-let count = 0;
+    let count = 0;
 
-// Define updateCounter function
-function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-}
+    function updateCounter() {
+        count++;
+        $('#count').text(`${count} clicks on the button`);
+    }
 
-// Bind updateCounter to button click event with debounce
-$('#start-button').on('click', _.debounce(updateCounter, 300));
+    $('#start-btn').on('click', _.debounce(updateCounter, 500));
+});
