22655a05d985298741c2ba59f25373fd5a61d03e
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-05-27 15:54:27 +0100

    Webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,46 +1,38 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'production', // Set mode to production
-    entry: './src/index.js', // Entry point for compilation
-    output: {
-        filename: 'bundle.js', // Output filename
-        path: path.resolve(__dirname, 'public'), // Output directory
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // Target JavaScript files
-                exclude: /node_modules/, // Exclude node_modules
-                use: {
-                    loader: 'babel-loader', // Use Babel loader
-                    options: {
-                        presets: ['@babel/preset-env'] // Use the env preset
-                    }
-                }
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
             },
-            {
-                test: /\.css$/, // Target CSS files
-                use: ['style-loader', 'css-loader'] // Use style-loader and css-loader
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/, // Target image files
-                use: [
-                    {
-                        loader: 'file-loader', // Use file-loader
-                        options: {
-                            name: '[name].[ext]', // Keep original file name and extension
-                            outputPath: 'images/', // Output directory for images
-                            esModule: false
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
-    },
+          },
+        ],
+      },
+    ],
+  },
 };
