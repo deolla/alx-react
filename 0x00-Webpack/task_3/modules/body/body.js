7fcf4ef10a3577a331ea1023b8ca172c3735f406
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-05-27 16:40:22 +0100

    Webpack

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+
+$('body').append("<button>Click here to get started</button>");
+$('body').append("<p id='count'></p>");
+
+// Initialize click counter.
+let count = 0;
+
+//Define updateCounter function
+function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+}
+
+// Bind updateCounter function to button click event with de
+$('button').on('click', _.debounce(updateCounter, 500));
+
