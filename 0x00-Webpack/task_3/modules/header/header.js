668034d85a461e6c033da4e54dc31094c4d9da00
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-05-27 16:43:21 +0100

    Webpack

--- 0x00-Webpack/task_3/modules/header/header.js
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import './header.css';
 
-$('body').append('<div id="logo"></div>')
-$('body').append('<h1>Holberton Dashboard</h1>')
+$('body').append('<header></header>')
+$('header').append('<div id="logo"></div>')
+$('header').append('<h1>Holberton Dashboard</h1>')
 console.log('Init header')
