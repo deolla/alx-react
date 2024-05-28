cd70cdcedb8070d837b275408d7953431d2e3ce9
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-05-28 21:08:38 +0100

    react

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import holbertonLogo from "./holberton-logo.png";
+import "./App.css";
+import {getFullYear, getFooterCopy} from "./utils";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
