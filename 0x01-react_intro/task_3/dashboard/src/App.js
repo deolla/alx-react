b97bcb226ff0aa55bd0d8e6334f87cf4a3fe6f0d
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-05-28 23:14:05 +0100

    react_tests

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import holbertonLogo from "./holberton-logo.png";
+import "./App.css";
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={holbertonLogo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
       </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password"></input>
+          <button type="submit">OK</button>
+        </form>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
+      </div>
     </div>
   );
 }
--- 0x01-react_intro/task_3/dashboard/src/index.js
+++ 0x01-react_intro/task_3/dashboard/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +10,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const root_notifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
+root_notifications.render(
+  <React.StrictMode>
+     <Notifications />
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
